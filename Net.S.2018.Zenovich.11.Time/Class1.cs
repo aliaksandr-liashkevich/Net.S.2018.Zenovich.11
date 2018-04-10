301c2711ca99e3e6caaa23fc9510c7a7a1522924
azenovich <alexander.zenovich@gmail.com>
2018-04-10 12:59:28 +0300

    Добавьте файлы проекта.

---
+++ Net.S.2018.Zenovich.11.Time/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Net.S._2018.Zenovich._11.Time
+{
+    public class Class1
+    {
+    }
+}
